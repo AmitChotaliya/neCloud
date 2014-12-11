e1e4022148849bc4d81221361b37653a3827b026
Amit Chotaliya <amitchotaliya@neevtech.com>
2014-12-11 15:52:45 +0530

    project setup

---
+++ src/main/java/neCloud/App.java
@@ -0,0 +1,13 @@
+package neCloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
